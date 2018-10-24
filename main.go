368d466f73fea1c7cba32844f5c610f954332702
Stanislav Siska <stanislav.siska@gmail.com>
2018-10-24 10:45:16 +0200

    Update message from Jenkins X Golang HTTP server

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X: Golang HTTP Server. Welcome!"
 
 	from := ""
 	if r.URL != nil {
